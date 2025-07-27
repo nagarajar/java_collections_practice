232389bc9a916794a8207d77ccba883ccbff6073
nagarajar <nagarajar@gmail.com>
2025-07-27 20:25:53 +0530

    Synchronizatio definition added

--- src/practice/java/collections/synchronization/MainClassForThreadsExec.java
+++ src/practice/java/collections/synchronization/MainClassForThreadsExec.java
@@ -2,6 +2,8 @@ package practice.java.collections.synchronization;
 
 public class MainClassForThreadsExec {
 	public static void main(String[] args) {
+		//Synchronization: When multiple threads are trying to perform operation on the same object, but if one thread 
+		//is performing some operation other threads has to wait till the current thread completes its execution.
 		//0. Thread - Thread is a independent path of execution
 		//1. Create the two thread class by implementing Runnable or extending Thread class
 		//2. Now create the object of those threads and then start the thread also run in debug mode observe the threads creation.
